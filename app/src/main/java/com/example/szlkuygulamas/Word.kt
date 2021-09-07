d2a11b7c3f238d5162d15c7865ced84975449301
gülce <gizemgulce@gmail.com>
2021-09-07 10:10:38 +0300

    functionality

---
+++ app/src/main/java/com/example/szlkuygulamas/Word.kt
@@ -0,0 +1,6 @@
+package com.example.szlkuygulamas
+
+
+data class Word(val id:Int,
+                val word:String,
+                val meaning:String)
---
+++ app/src/main/java/com/example/szlkuygulamas/WordService.kt
@@ -0,0 +1,9 @@
+package com.example.szlkuygulamas
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface WordService {
+    @GET("word_list.php")
+    fun getList(): Call<List<Word>>
+}
